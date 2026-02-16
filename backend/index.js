0ddc8591af47b9f03c55c613795cf511cb5c4807
MohammadSufian-max <39sufiyan@gmail.com>
2026-02-16 11:20:33 +0530

    fix port mismatch

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.status(200).send("HELLO FROM RAILWAY");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Running on", PORT);
