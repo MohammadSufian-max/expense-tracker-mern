8598ac841a0115d61f4538478c4c08d83857a44d
MohammadSufian-max <39sufiyan@gmail.com>
2026-02-16 11:44:45 +0530

    add db back

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,11 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-
     console.log("MongoDB Connected");
-  } catch (error) {
-    console.error("MongoDB Error:", error.message);
-    process.exit(1); // VERY IMPORTANT
+  } catch (err) {
+    console.error(err);
   }
 };
 
