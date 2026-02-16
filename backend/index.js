5f92a7cc6fcacebc6a75bc06f6008e6ee456919c
MohammadSufian-max <39sufiyan@gmail.com>
2026-02-16 10:04:20 +0530

    added root route and fixed port

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDb = require("./db/db")
-const userRouter = require('./router/userRouter')
-const expenseRouter = require('./router/expenseRouter')
+const connectDb = require("./db/db");
+const userRouter = require('./router/userRouter');
+const expenseRouter = require('./router/expenseRouter');
+
+dotenv.config(); // load env vars
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/auth',userRouter)
-app.use('/expenses',expenseRouter)
+
+// ROOT ROUTE (for Railway test)
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+app.use('/auth', userRouter);
+app.use('/expenses', expenseRouter);
+
 connectDb();
 
-const port = 4000 || process.env.PORT_NO ;
-app.listen(port , ()=>{
-        console.log(`Server on :- ${port}`);
-})
+// FIXED PORT
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Server on :- ${port}`);
+});
